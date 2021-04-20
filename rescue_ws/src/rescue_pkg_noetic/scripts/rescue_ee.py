da4fa240580613b9056e5fa4a89467152964772b
RHughes98 <ryan.hughes.md@gmail.com>
2021-04-20 13:50:03 -0600

    bare starter for EE node

---
+++ rescue_ws/src/rescue_pkg_noetic/scripts/rescue_ee.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# ROS framework / messages
+import rospy
+from std_msgs.msg import String
+from rescue_pkg_noetic.msg import location_command
+from rescue_pkg_noetic.msg import pan_tilt
